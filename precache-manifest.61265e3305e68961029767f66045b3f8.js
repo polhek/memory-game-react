2e852b0e78f8e104b56453128cfda89eedc1a223
polhek <ziga.groselj@gmail.com>
2020-10-23 13:52:03 +0200

    Updates

---
+++ precache-manifest.61265e3305e68961029767f66045b3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddc89dab5946702f38f6b8620954b7a",
+    "url": "/memory-game-react/index.html"
+  },
+  {
+    "revision": "186a3f649bc7fa94b8bc",
+    "url": "/memory-game-react/static/css/main.e1050963.chunk.css"
+  },
+  {
+    "revision": "41e9bbcda989bdd34ca9",
+    "url": "/memory-game-react/static/js/2.2b62368c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/memory-game-react/static/js/2.2b62368c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186a3f649bc7fa94b8bc",
+    "url": "/memory-game-react/static/js/main.9155d9d6.chunk.js"
+  },
+  {
+    "revision": "abb2c3db147a2ecc282d",
+    "url": "/memory-game-react/static/js/runtime-main.6f7790a6.js"
+  },
+  {
+    "revision": "d5746e3b20453d6cee99ab27a0bdf949",
+    "url": "/memory-game-react/static/media/Staatliches-Regular.d5746e3b.ttf"
+  }
+]);
